b879d027f7c78278f76edfba82895e01a8922651
YhoanGiraldo <166673914+YhoanGiraldo@users.noreply.github.com>
2025-10-04 23:42:22 -0500

    Add files via upload

---
+++ BLLUsuario.cs
@@ -0,0 +1,42 @@
+﻿
+
+using CapaDatos;
+using System.Data;
+
+namespace CapaNegocio
+{
+    public class BLLUsuario
+    {
+
+        private DALUsuarios objetoCD = new DALUsuarios();
+
+        public DataTable View()
+        {
+
+            DataTable tabla = new DataTable();
+            tabla = objetoCD.ReadAllUsuario();
+            return tabla;
+        }
+
+        public void Create(string usuario, string contrasena, int intentos, double nivelSeg, DateTime fechaReg)
+        {
+
+            objetoCD.CreateUsuario(usuario, contrasena, Convert.ToInt32(intentos), Convert.ToDouble(nivelSeg), fechaReg);
+        }
+
+
+        public void Update(string usuario, string contrasena, int intentos, double nivelSeg, DateTime fechaReg, int id)
+        {
+            objetoCD.UpdateUsuario(usuario, contrasena, Convert.ToInt32(intentos), Convert.ToDouble(nivelSeg), fechaReg, Convert.ToInt32(id));
+        }
+
+
+        public void Delete(int id)
+        {
+
+            objetoCD.DeleteUsuario(id);
+        }
+
+
+    }
+}
---
+++ DALConexion.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class DALConexion
+    {
+        private SqlConnection Conexion = new
+            SqlConnection("Data Source= DESKTOP-FDFOE8V\\SQLEXPRESS;Initial Catalog=BD_TEST;Integrated Security=True;Encrypt=False;");
+
+       
+        public SqlConnection OpenConnection()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+
+        
+        public SqlConnection CloseConnection()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
